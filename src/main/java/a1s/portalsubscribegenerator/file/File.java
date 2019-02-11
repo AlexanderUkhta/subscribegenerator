bcdf9d6923371b2048ad3ffe0f512bff32434170
t.umnova <tanyaumnova1994@gmail.com>
2019-02-11 17:11:36 +0300

    можно получать значения из файла

--- src/main/java/a1s/portalsubscribegenerator/file/File.java
+++ src/main/java/a1s/portalsubscribegenerator/file/File.java
@@ -1,16 +1,19 @@
 package a1s.portalsubscribegenerator.file;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class Read {
+public class File {
     private static final String file = "src/main/resources/Пример настроек рассылок.xlsx";
     private XSSFWorkbook book = setBook();
     private XSSFSheet connectionSheet = setConnectionSheet();
-    private XSSFSheet subscriptionSheet = setConnectionSheet();
+    private XSSFSheet subscriptionSheet = setSubscriptionSheet();
 
     private XSSFWorkbook setBook() {
         XSSFWorkbook workbook;
@@ -45,4 +48,42 @@ public class Read {
         return sheet;
     }
 
+    private Cell getCell(int r, int c, XSSFSheet sheet) {
+        Row row = sheet.getRow(r);
+        if (row == null) {
+            row = sheet.createRow(r);
+        }
+        Cell cell = row.getCell(c);
+        if (cell == null) {
+            cell = row.createCell(c);
+        }
+        cell.removeCellComment();
+        return cell;
+    }
+
+    public String getValue(int r, int c, XSSFSheet sheet) {
+        Cell cell = getCell(r, c, sheet);
+        if(cell == null) {
+            cell = sheet.getRow(r).createCell(c);
+        }
+        cell.removeCellComment();
+        String res;
+        if(cell.getCellTypeEnum() == CellType.STRING){
+            res = cell.getStringCellValue().trim();
+        } else if(cell.getCellTypeEnum() == CellType.NUMERIC) {
+            int n = (int) cell.getNumericCellValue();
+            res = String.valueOf(n);
+        } else {
+            res = "0";
+        }
+        return res;
+    }
+
+    public XSSFSheet getConnectionSheet() {
+        return connectionSheet;
+    }
+
+    public XSSFSheet getSubscriptionSheet() {
+        return subscriptionSheet;
+    }
 }
