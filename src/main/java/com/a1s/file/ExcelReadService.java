2eb78bf5ce94a9c1ffd7ca5c49f1194de5a2dc6d
t.umnova <tanyaumnova1994@gmail.com>
2019-03-15 18:10:38 +0300

    исправлено получение текста из ячеек, содержщих формулы

--- src/main/java/com/a1s/file/ExcelReadService.java
+++ src/main/java/com/a1s/file/ExcelReadService.java
@@ -1,10 +1,8 @@
 package com.a1s.file;
 
 import com.a1s.subscribegeneratorapp.model.SubscribeRequest;
-import com.a1s.subscribegeneratorapp.model.Subscription;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,57 +12,43 @@ import java.util.TreeMap;
 @Service
 public class ExcelReadService {
     private static final Log logger = LogFactory.getLog(ExcelReadService.class);
-    private int psIdColumn;
-    private int shortNumColumn;
-    private int textRequestColumn;
-    private int welcomeNotificationColumn;
 
     @Autowired
-    private File file;
-    {
-        getColumnsId();
-    }
-
-    /**
-     * Получение id столбцов с необходимыми данными для теста
-     */
-    private void getColumnsId() {
-        int row = 0;
-        XSSFSheet sheet = file.getSheet("Подключение");
-        int lastCell = file.getLastCellId(sheet);
-        for(int i = 0; i < lastCell; i++) {
-            String text = file.getCellValue(row, i, file.getSheet("Подключение"));
-            if(text.equals("ps id")) {
-                psIdColumn = i;
-            } else if(text.equals("Короткий номер")) {
-                shortNumColumn = i;
-            } else if(text.equals("Текст сообщения")) {
-                textRequestColumn = i;
-            }
-        }
-        for (int i = 0; i < file.getLastCellId(file.getSheet("Рассылки")); i++) {
-            String text = file.getCellValue(row, i, file.getSheet("Рассылки"));
-            if(text.equals("Уведомление при подключении")) {
-                welcomeNotificationColumn = i;
-                break;
-            }
-        }
-    }
+    private File file = new File();
 
     public Map<Integer, SubscribeRequest> findAll() {
         Map<Integer, SubscribeRequest> treeMap = new TreeMap<>();
         for(int i = 1; i < file.getLastRowId(file.getSheet("Подключение")); i++) {
-            int psid = Integer.parseInt(file.getCellValue(i, psIdColumn, file.getSheet("Подключение")));
-            String shortNum = file.getCellValue(i, shortNumColumn, file.getSheet("Подключение"));
-            String textRequest = file.getCellValue(i, textRequestColumn, file.getSheet("Подключение"));
-            String welcomeNotification = file.getCellValue(i, welcomeNotificationColumn, file.getSheet("Рассылки"));
+            int psid = Integer.parseInt(file.getCellValue(i, file.getCellId("ps id", file.getSheet("Подключение")),
+                    file.getSheet("Подключение")));
             if (psid == 0) {
                 logger.warn("Empty ps id in row " + i + 1);
             } else {
-                treeMap.put(i, new SubscribeRequest(i, psid, String.valueOf(psid), shortNum, textRequest, welcomeNotification));
+                String shortNum = file.getCellValue(i, file.getCellId("Короткий номер", file.getSheet("Подключение")),
+                        file.getSheet("Подключение"));
+                String textRequest = file.getCellValue(i, file.getCellId("Текст сообщения", file.getSheet("Подключение")),
+                        file.getSheet("Подключение"));
+                String welcomeNotification = file.getCellValue(findRow(psid), file.getCellId("Уведомление при подключении", file.getSheet("Рассылки")),
+                        file.getSheet("Рассылки"));
+                String subscriptionName = file.getCellValue(findRow(psid), file.getCellId("Название рассылки", file.getSheet("Рассылки")),
+                        file.getSheet("Рассылки"));
+                treeMap.put(i, new SubscribeRequest(i, psid, subscriptionName, shortNum, textRequest, welcomeNotification));
             }
         }
         return treeMap;
+    }
+
+    private int findRow(int psId) {
+        int row = 0;
+        for (int i = 1; i < file.getLastRowId(file.getSheet("Рассылки")); i++) {
+            int id = Integer.valueOf(file.getCellValue(i, file.getCellId("ps id", file.getSheet("Рассылки")),
+                    file.getSheet("Рассылки")));
+            if(psId == id) {
+                row = i;
+                break;
+            }
+        }
+        return row;
     }
 
     public void sout() {
--- src/test/java/filetest/FileTest.java
+++ src/test/java/filetest/FileTest.java
@@ -1,8 +1,10 @@
 package filetest;
 
 import com.a1s.file.ExcelReadService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FileTest {
+
     @org.junit.Test
     public void test() {
         ExcelReadService read = new ExcelReadService();
